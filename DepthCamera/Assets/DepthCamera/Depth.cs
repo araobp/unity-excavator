624564a8af792ac3d0fc8a9af951fdc134295ea4
arao <araobp@users.noreply.github.com>
2020-06-24 20:25:41 +0900

    Depth camera

---
+++ DepthCamera/Assets/DepthCamera/Depth.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Depth : MonoBehaviour
+{
+    public Material mat;
+
+    // Capture an image from a camera
+    public void Capture()
+    {
+        Camera camera = GetComponent<Camera>();
+        RenderTexture activeRenderTexture = RenderTexture.active;
+        RenderTexture.active = camera.targetTexture;
+
+        camera.Render();
+
+        try
+        {
+            Texture2D image = new Texture2D(camera.targetTexture.width, camera.targetTexture.height);
+            image.ReadPixels(new Rect(0, 0, camera.targetTexture.width, camera.targetTexture.height), 0, 0);
+            image.Apply();
+            RenderTexture.active = activeRenderTexture;
+
+            byte[] bytes = image.EncodeToJPG();
+            Destroy(image);
+            File.WriteAllBytes($"{Application.dataPath}/Capture/depth.png", bytes);
+        } catch (NullReferenceException e)
+        {
+        }
+    }
+
+    public void Start()
+    {
+        GetComponent<Camera>().depthTextureMode |= DepthTextureMode.Depth;
+    }
+
+    public void OnRenderImage(RenderTexture source, RenderTexture dest)
+    {
+        Graphics.Blit(source, dest, mat);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C)) Capture();
+    }
+}
---
+++ DepthCamera/Assets/DepthCamera/depth.shader
@@ -0,0 +1,46 @@
+﻿Shader "Unlit/shader"
+{
+	SubShader
+	{
+		Tags { "RenderType" = "Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#include "UnityCG.cginc"
+
+			sampler2D _CameraDepthTexture;
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float4 vertex : SV_POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			v2f vert(appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+				return o;
+			}
+
+			fixed4 frag(v2f i) : SV_Target
+			{
+				fixed depth = SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture,i.uv);
+     			depth = Linear01Depth(depth);
+				return fixed4(1-depth,1-depth,1-depth,1);
+			}
+			ENDCG
+		}
+	}
+}
