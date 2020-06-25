90d15ab4efd2208a91af520d69e7a78dcd3e3484
arao <araobp@users.noreply.github.com>
2020-06-25 20:55:35 +0900

    update

---
+++ BasicClassicalPhysics/Assets/MenuSinglePendulum.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSinglePendulum : MonoBehaviour
+{
+
+    public void OnSinglePendulumClick()
+    {
+        SceneManager.LoadScene("Pendulum");
+    }
+}
