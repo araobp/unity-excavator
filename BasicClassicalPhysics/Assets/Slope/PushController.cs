3d06eaf339c520ad832319f8ef0a9de9a82fc5f7
arao <araobp@users.noreply.github.com>
2020-06-27 01:33:07 +0900

    Velocity vector

--- BasicClassicalPhysics/Assets/Slope/PushController.cs
+++ BasicClassicalPhysics/Assets/Slope/PushController.cs
@@ -9,6 +9,23 @@ public class PushController : MonoBehaviour
     private Rigidbody rb;
     private Rigidbody rb2;
 
+    public GameObject arrowPrefab;
+
+    private GameObject arrow;
+    private GameObject arrow2;
+
+    void OrientVector(GameObject arrow, Transform transform, Vector3 vector)
+    {
+        Vector3 startPoint = transform.position;
+        Vector3 endPoint = vector + transform.position;
+        Vector3 direction = endPoint - startPoint;
+
+        float length = direction.magnitude * 10F - 2F;  // 1/10 scale, subtract the cone length
+        arrow.transform.position = endPoint;
+        arrow.transform.GetChild(1).transform.localScale = new Vector3(1F, 1F, length);
+        arrow.transform.LookAt(transform);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -31,12 +48,20 @@ public class PushController : MonoBehaviour
         textSlope.text = $"Inclination: {360F - slope.rotation.eulerAngles.z} deg";
         textSlope2.text = $"Inclination: {360F - slope2.rotation.eulerAngles.z} deg";
 
+        arrow = Instantiate(arrowPrefab);
+        arrow2 = Instantiate(arrowPrefab);
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.AddForce(-rb.transform.right * rb.mass * acceleration, ForceMode.Force);
-        rb2.AddForce(-rb2.transform.right * rb2.mass * acceleration, ForceMode.Force);
+        Vector3 force = rb.mass * acceleration * -rb.transform.right;
+        Vector3 force2 = rb2.mass * acceleration * -rb2.transform.right;
+
+        rb.AddForce(force, ForceMode.Force);
+        rb2.AddForce(force2, ForceMode.Force);
+
+        OrientVector(arrow, rb.transform, force/1000F);
+        OrientVector(arrow2, rb2.transform, force2/1000F);
     }
 }
