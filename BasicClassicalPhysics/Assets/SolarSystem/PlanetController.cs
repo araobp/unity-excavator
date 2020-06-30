b9c3f789b67a2252ad68d5966159c8d0fab51f4f
arao <araobp@users.noreply.github.com>
2020-06-30 18:31:17 +0900

    update

---
+++ BasicClassicalPhysics/Assets/SolarSystem/PlanetController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetController : MonoBehaviour
+{
+    public float initialSpeed = 20F;
+    public float rotation = 0F;
+    public float angularVelocity = 0.5F;
+
+    GameObject star;
+    float starMass;
+    float planetMass;
+    Rigidbody rb;
+
+    const float G = 6.674F;  // This is 10^11 times larger than the real constant of gravitatioin. 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        star = GameObject.Find("Star");
+        starMass = star.GetComponent<Rigidbody>().mass;
+        rb = gameObject.GetComponent<Rigidbody>();
+        planetMass = rb.mass;
+        transform.Rotate(rotation, 0, 0);
+        rb.angularVelocity = new Vector3(0, angularVelocity, 0);
+        Vector3 initialForward = new Vector3(rb.transform.forward.x, rb.transform.forward.y, rb.transform.forward.z);
+        rb.velocity = new Vector3(initialForward.x, initialForward.y, initialForward.z) * initialSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 r = star.transform.position - transform.position;
+        float gravity = G * starMass * planetMass / Mathf.Pow(r.magnitude, 2F);
+        rb.AddForce(gravity * r.normalized, ForceMode.Force);
+    }
+}
---
+++ BasicClassicalPhysics/Assets/Torque/TorqueControl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TorqueControl : MonoBehaviour
+{
+    public float force = 1F;
+    private Rigidbody rb;
+
+    private float startTime;
+
+    private float momentOfInertia;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        rb.maxAngularVelocity = 10000000F;
+        startTime = Time.time;
+
+        // [Reference] http://hyperphysics.phy-astr.gsu.edu/hbase/mi.html#mi
+        momentOfInertia = 2F / 5F * rb.mass * Mathf.Pow(rb.transform.localScale.x / 2F, 2F);
+    }
+
+    bool stopPowering = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // [Reference] http://hyperphysics.phy-astr.gsu.edu/hbase/rke.html
+        float rotationalEnergy = 1F / 2F * momentOfInertia * Mathf.Pow(rb.angularVelocity.magnitude, 2F);
+
+        if (!stopPowering)
+        {
+            if (rotationalEnergy >= 20F)
+            {
+                stopPowering = true;
+                float elapsedTime = Time.time - startTime;
+                Debug.Log($"Elapsed time: {elapsedTime} for {gameObject.name}");
+            }
+            else
+            {
+                rb.AddTorque(transform.up * force);
+            }
+        }
+    }
+}
