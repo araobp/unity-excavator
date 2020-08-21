52380f1fec913f3f5d3d1315ebc3826f9881703a
arao <araobp@users.noreply.github.com>
2020-08-22 05:40:13 +0900

    update

---
+++ AGV/Assets/AGV/HazardArea.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardArea: MonoBehaviour
+{
+    AgvController controller;
+    bool red;
+
+    public void SetController(AgvController controller, bool red)
+    {
+        this.controller = controller;
+        this.red = red;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "vBeaconEnable")
+        {
+            GameObject parent = transform.parent.gameObject;
+            AgvController controller = parent.GetComponent<AgvController>();
+            this.controller.OnHazardEnter(red);
+        }
+    }
+}
