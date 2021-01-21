aa55ac7964e4d6fde9f5595b93b020201f989f2f
arao <araobp@users.noreply.github.com>
2021-01-21 12:44:10 +0900

    update

---
+++ RenderStreaming/Assets/WallClock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WallClock : MonoBehaviour
+{
+    Text clock;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        clock = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var timeString = DateTime.Now.ToString("HH:mm:ss.fff");
+        clock.text = $"{timeString}";
+    }
+}
