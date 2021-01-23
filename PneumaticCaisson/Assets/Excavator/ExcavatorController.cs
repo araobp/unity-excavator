50442b1916294930257a4a8e7a8f2407f94e3d7a
arao <araobp@users.noreply.github.com>
2021-01-23 20:52:04 +0900

    update

--- PneumaticCaisson/Assets/Excavator/ExcavatorController.cs
+++ PneumaticCaisson/Assets/Excavator/ExcavatorController.cs
@@ -8,25 +8,30 @@ public class ExcavatorController : MonoBehaviour
     private const float ROTATION_SPEED = 20F;
     private const float RUNNING_SPEED = 1F;
 
-    private GameObject runningAxis;
-    private GameObject engineAxis;
-    private GameObject stickBone;
-    private GameObject bucketAxis;
+    const string RUNNING_AXIS = "Bone/RunningAxis";
+    const string ENGINE_AXIS = RUNNING_AXIS + "/Bone.002/EngineAxis";
+    const string STICK_BONE = ENGINE_AXIS + "/Bone.004/StickBone";
+    const string BUCKET_AXIS = STICK_BONE + "/Bone.014/Bone.016/BucketAxis";
 
-    private GameObject engineRightCylinder;
-    private GameObject engineLeftCylinder;
-    private GameObject engineLeftCylinderTarget;
-    private GameObject engineRightCylinderTarget;
+    private Transform runningAxis;
+    private Transform engineAxis;
+    private Transform stickBone;
+    private Transform bucketAxis;
 
-    private GameObject armLeftCylinderTarget;
-    private GameObject armRightCylinderTarget;
-    private GameObject armRightCylinder;
-    private GameObject armLeftCylinder;
+    private Transform engineRightCylinder;
+    private Transform engineLeftCylinder;
+    private Transform engineLeftCylinderTarget;
+    private Transform engineRightCylinderTarget;
 
-    private GameObject bucketCylinderA;
-    private GameObject bucketCylinderB;
-    private GameObject bucketCylinderATarget;
-    private GameObject bucketCylinderBTarget;
+    private Transform armLeftCylinderTarget;
+    private Transform armRightCylinderTarget;
+    private Transform armRightCylinder;
+    private Transform armLeftCylinder;
+
+    private Transform bucketCylinderA;
+    private Transform bucketCylinderB;
+    private Transform bucketCylinderATarget;
+    private Transform bucketCylinderBTarget;
 
     void OrientBucketHydraulicCylinder()
     {
@@ -53,80 +58,85 @@ public class ExcavatorController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        runningAxis = GameObject.Find("RunningAxis") as GameObject;
-        engineAxis = GameObject.Find("EngineAxis") as GameObject;
-        stickBone = GameObject.Find("StickBone");
-        bucketAxis = GameObject.Find("BucketAxis");
+        runningAxis = transform.Find(RUNNING_AXIS);
+        engineAxis = transform.Find(ENGINE_AXIS);
+        stickBone = transform.Find(STICK_BONE);
+        bucketAxis = transform.Find(BUCKET_AXIS);
 
-        bucketCylinderA = GameObject.Find("BucketCylinderA");
-        bucketCylinderB = GameObject.Find("BucketCylinderB");
-        bucketCylinderATarget = GameObject.Find("BucketCylinderATarget");
-        bucketCylinderBTarget = GameObject.Find("BucketCylinderBTarget");
+        bucketCylinderA = transform.Find(BUCKET_AXIS + "/BucketCylinderA");
+        bucketCylinderB = transform.Find(STICK_BONE + "/Bone.014/BucketCylinderB");
+        bucketCylinderATarget = transform.Find(STICK_BONE + "/Bone.014/BucketCylinderB/BucketCylinderATarget");
+        bucketCylinderBTarget = transform.Find(BUCKET_AXIS + "/BucketCylinderA/BucketCylinderBTarget");
 
-        engineRightCylinder = GameObject.Find("EngineRightCylinder");
-        engineLeftCylinder = GameObject.Find("EngineLeftCylinder");
-        engineLeftCylinderTarget = GameObject.Find("EngineLeftCylinderTarget");
-        engineRightCylinderTarget = GameObject.Find("EngineRightCylinderTarget");
+        engineRightCylinder = transform.Find(RUNNING_AXIS + "/Bone.005/EngineRightCylinder");
+        engineLeftCylinder = transform.Find(RUNNING_AXIS + "/Bone.006/EngineLeftCylinder");
+        engineLeftCylinderTarget = transform.Find(ENGINE_AXIS + "/EngineLeftCylinderTarget");
+        engineRightCylinderTarget = transform.Find(ENGINE_AXIS + "EngineRightCylinderTarget");
 
-        armLeftCylinderTarget = GameObject.Find("ArmLeftCylinderTarget");
-        armRightCylinderTarget = GameObject.Find("ArmRightCylinderTarget");
-        armRightCylinder = GameObject.Find("ArmRightCylinder");
-        armLeftCylinder = GameObject.Find("ArmLeftCylinder");
+        armLeftCylinderTarget = transform.Find(RUNNING_AXIS + "/ArmLeftCylinderTarget");
+        armRightCylinderTarget = transform.Find(RUNNING_AXIS + "/ArmRightCylinderTarget");
+        armRightCylinder = transform.Find(ENGINE_AXIS + "/Bone.004/Bone.007/ArmRightCylinder");
+        armLeftCylinder = transform.Find(ENGINE_AXIS + "/Bone.004/Bone.008/ArmLeftCylinder");
+    }
 
-}
-
-// Update is called once per frame
-void Update()
+    // Update is called once per frame
+    void Update()
     {
         float rotation = ROTATION_SPEED * Time.deltaTime;
         float running = RUNNING_SPEED * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.G))
         {
-            runningAxis.transform.Rotate(new Vector3(0, rotation, 0));
-        } else if (Input.GetKey(KeyCode.J))
+            runningAxis.Rotate(new Vector3(0, rotation, 0));
+        }
+        else if (Input.GetKey(KeyCode.J))
         {
-            runningAxis.transform.Rotate(new Vector3(0, -rotation, 0));
-        } else if (Input.GetKey(KeyCode.U))
+            runningAxis.Rotate(new Vector3(0, -rotation, 0));
+        }
+        else if (Input.GetKey(KeyCode.U))
         {
             var pos = new Vector3(-running * 2F, 0, 0);
             transform.Translate(pos);
-        } else if (Input.GetKey(KeyCode.N))
+        }
+        else if (Input.GetKey(KeyCode.N))
         {
             var pos = new Vector3(running * 2F, 0, 0);
             transform.Translate(pos);
         }
         else if (Input.GetKey(KeyCode.Y))
         {
-            engineAxis.transform.Rotate(new Vector3(rotation * 0.5F, 0, 0));
+            engineAxis.Rotate(new Vector3(rotation * 0.5F, 0, 0));
             OrientArmHydraulicCylinder();
-        } else if (Input.GetKey(KeyCode.B))
+        }
+        else if (Input.GetKey(KeyCode.B))
         {
-            engineAxis.transform.Rotate(new Vector3(-rotation * 0.5F, 0, 0));
+            engineAxis.Rotate(new Vector3(-rotation * 0.5F, 0, 0));
             OrientArmHydraulicCylinder();
-        } else if (Input.GetKey(KeyCode.I))
+        }
+        else if (Input.GetKey(KeyCode.I))
         {
-            if (stickBone.transform.localPosition.y <= 0.024)
+            if (stickBone.localPosition.y <= 0.024)
             {
-                stickBone.transform.Translate(new Vector3(0, running * 0.5F, 0));
+                stickBone.Translate(new Vector3(0, running * 0.5F, 0));
             }
             Debug.Log("Stickbone: " + stickBone.transform.localPosition.y);
-        } else if (Input.GetKey(KeyCode.M))
+        }
+        else if (Input.GetKey(KeyCode.M))
         {
-            if (stickBone.transform.localPosition.y >= 0.0094)
+            if (stickBone.localPosition.y >= 0.0094)
             {
-                stickBone.transform.Translate(new Vector3(0, -running * 0.5F, 0));
+                stickBone.Translate(new Vector3(0, -running * 0.5F, 0));
             }
             Debug.Log("Stickbone: " + stickBone.transform.localPosition.y);
         }
         else if (Input.GetKey(KeyCode.L))
         {
-            bucketAxis.transform.Rotate(new Vector3(rotation * 1.5F, 0, 0));
+            bucketAxis.Rotate(new Vector3(rotation * 1.5F, 0, 0));
             OrientBucketHydraulicCylinder();
         }
         else if (Input.GetKey(KeyCode.K))
         {
-            bucketAxis.transform.Rotate(new Vector3(-rotation * 1.5F, 0, 0));
+            bucketAxis.Rotate(new Vector3(-rotation * 1.5F, 0, 0));
             OrientBucketHydraulicCylinder();
         }
 
@@ -134,16 +144,17 @@ void Update()
         if (joystickLeftX != 0)
         {
             var rot = -rotation * joystickLeftX;
-            runningAxis.transform.Rotate(new Vector3(0, rot, 0));
+            runningAxis.Rotate(new Vector3(0, rot, 0));
         }
 
         float joystickLeftY = Input.GetAxis("JoystickLeftY");
         if (joystickLeftY != 0)
         {
-            var y = stickBone.transform.localPosition.y;
+            var y = stickBone.localPosition.y;
             var run = -running * joystickLeftY;
-            if ((run <= 0 && y >= 0.0094) || (run > 0 && y <= 0.024)) {
-                stickBone.transform.Translate(new Vector3(0, run * 0.5F, 0));
+            if ((run <= 0 && y >= 0.0094) || (run > 0 && y <= 0.024))
+            {
+                stickBone.Translate(new Vector3(0, run * 0.5F, 0));
             }
         }
 
@@ -151,7 +162,7 @@ void Update()
         if (joystickRightX != 0)
         {
             var rot = -rotation * joystickRightX;
-            bucketAxis.transform.Rotate(new Vector3(rot * 2F, 0, 0));
+            bucketAxis.Rotate(new Vector3(rot * 2F, 0, 0));
             OrientBucketHydraulicCylinder();
         }
 
@@ -159,19 +170,21 @@ void Update()
         if (joystickRightY != 0)
         {
             var rot = rotation * joystickRightY;
-            engineAxis.transform.Rotate(new Vector3(rot * 0.5F, 0, 0));
+            engineAxis.Rotate(new Vector3(rot * 0.5F, 0, 0));
             OrientArmHydraulicCylinder();
         }
 
-        if (Input.GetKey(KeyCode.Joystick1Button0)) {
+        if (Input.GetKey(KeyCode.Joystick1Button0))
+        {
             var pos = new Vector3(running * 2F, 0, 0);
             transform.Translate(pos);
         }
 
-        else if (Input.GetKey(KeyCode.Joystick1Button3)) {
+        else if (Input.GetKey(KeyCode.Joystick1Button3))
+        {
             var pos = new Vector3(-running * 2F, 0, 0);
             transform.Translate(pos);
         }
 
     }
-} 
+}
---
+++ PneumaticCaisson/Assets/Mixamo/Action.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Action : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();       
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        animator.SetTrigger("FallDown");
+    }
+
+}
