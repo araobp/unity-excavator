5f149995824834a05a5ad0fc1a525a659838230e
arao <araobp@users.noreply.github.com>
2021-01-23 23:28:52 +0900

    update

---
+++ RenderStreaming/Assets/TestTouchEvents.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTouchEvents : MonoBehaviour
+{
+    public void TouchstartX()
+    {
+        Debug.Log("touchstart:X");
+    }
+    
+    public void TouchendX()
+    {
+        Debug.Log("touchend:X");
+    }
+
+    public void TouchstartA()
+    {
+        Debug.Log("touchstart:A");
+    }
+
+    public void TouchendA()
+    {
+        Debug.Log("touchend:A");
+    }
+
+    public void TouchstartY()
+    {
+        Debug.Log("touchstart:Y");
+    }
+
+    public void TouchendY()
+    {
+        Debug.Log("touchend:Y");
+    }
+
+    public void TouchstartB()
+    {
+        Debug.Log("touchstart:B");
+    }
+
+    public void TouchendB()
+    {
+        Debug.Log("touchend:B");
+    }
+}
