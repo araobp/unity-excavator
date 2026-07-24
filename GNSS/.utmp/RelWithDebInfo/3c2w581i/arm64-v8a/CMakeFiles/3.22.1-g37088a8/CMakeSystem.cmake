67af8e8500acd86bb87ba218cfd482850cc960ab
araobp <araobp@users.noreply.github.com>
2026-07-24 20:07:51 +0900

    update

---
+++ GNSS/.utmp/RelWithDebInfo/3c2w581i/arm64-v8a/CMakeFiles/3.22.1-g37088a8/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Darwin-25.3.0")
+set(CMAKE_HOST_SYSTEM_NAME "Darwin")
+set(CMAKE_HOST_SYSTEM_VERSION "25.3.0")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "arm64")
+
+include("/Applications/Unity/Hub/Editor/2020.3.3f1/6000.5.3f1/PlaybackEngines/AndroidPlayer/NDK/build/cmake/android.toolchain.cmake")
+
+set(CMAKE_SYSTEM "Android-1")
+set(CMAKE_SYSTEM_NAME "Android")
+set(CMAKE_SYSTEM_VERSION "1")
+set(CMAKE_SYSTEM_PROCESSOR "aarch64")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
